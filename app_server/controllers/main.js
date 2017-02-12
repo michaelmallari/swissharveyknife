5edf1bde4a7cdc9b30521b3fce14be7a756ede59
Michael Mallari <michael@michaelmallari.com>
2017-02-12 01:00:54 -0500

    Updated content

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,6 +1,30 @@
 // GET Home page - http://swissharveyknife.herokuapp.com
-module.exports.index = function(req, res, next) {
+module.exports.p1 = function(req, res, next) {
 	res.render('default', {
+		meta: {
+			title: 'Swiss Harvey Knife',
+			description: 'Prototype for Team Swiss Harvey Knife for the NYC Media Lab + MLBAM hackathon on February 10-12, 2017 at NYU.',
+			url_base,
+			url_path: '/'
+		}
+	});
+};
+
+// GET Home page - http://swissharveyknife.herokuapp.com
+module.exports.p2 = function(req, res, next) {
+	res.render('p2', {
+		meta: {
+			title: 'Swiss Harvey Knife',
+			description: 'Prototype for Team Swiss Harvey Knife for the NYC Media Lab + MLBAM hackathon on February 10-12, 2017 at NYU.',
+			url_base,
+			url_path: '/'
+		}
+	});
+};
+
+// GET Home page - http://swissharveyknife.herokuapp.com
+module.exports.p3 = function(req, res, next) {
+	res.render('p3', {
 		meta: {
 			title: 'Swiss Harvey Knife',
 			description: 'Prototype for Team Swiss Harvey Knife for the NYC Media Lab + MLBAM hackathon on February 10-12, 2017 at NYU.',
--- app_server/routes/main.js
+++ app_server/routes/main.js
@@ -3,6 +3,12 @@ var router = express.Router();
 var ctrlMain = require('../controllers/main');
 
 // GET Home page - http://swissharveyknife.herokuapp.com
-router.get('/', ctrlMain.index);
+router.get('/', ctrlMain.p1);
+
+// GET page 2 - http://swissharveyknife.herokuapp.com/2
+router.get('/2', ctrlMain.p2);
+
+// GET page 3 - http://swissharveyknife.herokuapp.com/3
+router.get('/3', ctrlMain.p3);
 
 module.exports = router;
